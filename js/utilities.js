199d66b3f82f8cfbd938b971521f1136a110aaa1
Blaine Adams <blaine.adams@geofac.com>
2016-11-02 11:45:22 -0500

    created a new footer.html template, added a small utilities js file for custom actions, updated HTML files to use footer with content update

---
+++ js/utilities.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Blaine Adams on 11/2/16.
+ *
+ * Copyright (c) 2016 Blaine Adams
+ * License: MIT
+ *
+ */
+
+$(function(){
+	$(".siteFooter").load("footer.html");
+	console.log('ok');
+});
